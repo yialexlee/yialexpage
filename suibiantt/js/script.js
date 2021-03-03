4e1af01d4e33e53f56cba68866bd80e0fb052503
涛浪浮萍yialex lee <44304753+yialexlee@users.noreply.github.com>
2021-03-03 13:28:53 +0800

    Update script.js

--- suibiantt/js/script.js
+++ suibiantt/js/script.js
@@ -2925,6 +2925,14 @@ cover: 'img/kagirinakuhaiiroesorii.jpg',
 mp3: '',
 ogg: 'mp3/kagirinakuhaiiroesorii.ogg'
 },
+{
+title: 'I Need Your Love-Nightcore',
+artist: 'Madilyn Bailey, Jake Coco',
+album: '',
+cover: 'img/ineedyourlovejake.jpg',
+mp3: '',
+ogg: 'mp3/ineedyourlovejake.ogg'
+},
 ];
 
 	// Load playlist
