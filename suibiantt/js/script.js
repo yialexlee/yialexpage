9f4520484bc8116d6f53a1e971c037b93101172d
涛浪浮萍yialex lee <44304753+yialexlee@users.noreply.github.com>
2021-03-09 05:11:02 +0800

    Update script.js

--- suibiantt/js/script.js
+++ suibiantt/js/script.js
@@ -3046,6 +3046,22 @@ cover: 'img/ruoshuisanqian.jpg',
 mp3: '',
 ogg: 'mp3/ruoshuisanqian.ogg'
 },
+{
+title: '3!4!',
+artist: 'SOYA (소야) & DinDin (딘딘)',
+album: 'Hello, Me! (안녕? 나야!) OST Part 3',
+cover: 'img/34soyadindin.jpg',
+mp3: '',
+ogg: 'mp3/34soyadindin.ogg'
+},
+{
+title: 'ケダモノのフレンズ',
+artist: 'にしな Nishina',
+album: '',
+cover: 'img/kedamonofurensunishina.jpg',
+mp3: '',
+ogg: 'mp3/kedamonofurensunishina.ogg'
+},
 ];
 
 	// Load playlist
