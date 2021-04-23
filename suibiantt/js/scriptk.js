e8c765bdd0f929716d4fa657d174b6ae25609676
涛浪浮萍yialex lee <44304753+yialexlee@users.noreply.github.com>
2021-04-24 02:18:48 +0800

    Update scriptk.js

--- suibiantt/js/scriptk.js
+++ suibiantt/js/scriptk.js
@@ -1030,6 +1030,14 @@ cover: 'img/dadadalunarsolar.jpg',
 mp3: '',
 ogg: 'mp3/dadadalunarsolar.ogg'
 },
+{
+title: 'We are',
+artist: '이승윤(LEE SEUNG YOON)',
+album: '〈로스쿨(LAW SCHOOL)〉 OST Part.1',
+cover: 'img/weareleeseungyoon.jpg',
+mp3: '',
+ogg: 'mp3/weareleeseungyoon.ogg'
+},
 ];
 
 	// Load playlist
