ffeb1093977641aa0d2903f75505b88db34e174f
涛浪浮萍yialex lee <44304753+yialexlee@users.noreply.github.com>
2021-05-09 03:51:16 +0800

    Update scriptj.js

--- suibiantt/js/scriptj.js
+++ suibiantt/js/scriptj.js
@@ -1557,6 +1557,30 @@ cover: 'img/checkmatemilet.jpg',
 mp3: '',
 ogg: 'mp3/checkmatemilet.ogg'
 },
+{
+title: 'Ever after',
+artist: 'Aimer',
+album: '',
+cover: 'img/everafteraimer.jpg',
+mp3: '',
+ogg: 'mp3/everafteraimer.ogg'
+},
+{
+title: '愛を知るまでは',
+artist: 'あいみょん',
+album: '',
+cover: 'img/aiwoshirumadewaaimiyon.jpg',
+mp3: '',
+ogg: 'mp3/aiwoshirumadewaaimiyon.ogg'
+},
+{
+title: 'Cry Baby',
+artist: '髭男dism',
+album: 'Cry Baby',
+cover: 'img/crybabydism.jpg',
+mp3: '',
+ogg: 'mp3/crybabydism.ogg'
+},
 ];
 
 	// Load playlist
