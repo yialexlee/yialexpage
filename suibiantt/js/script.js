7a82116b92d8dc354d43c012854480b9f6c16243
涛浪浮萍yialex lee <44304753+yialexlee@users.noreply.github.com>
2021-05-17 09:30:21 +0800

    Update script.js

--- suibiantt/js/script.js
+++ suibiantt/js/script.js
@@ -3961,7 +3961,7 @@ ogg: 'mp3/runawaysimondominic.ogg'
 {
 title: 'Giant',
 artist: 'YUQI ((G)I-DLE)',
-album: '',
+album: 'A Page',
 cover: 'img/giantyuqi.jpg',
 mp3: '',
 ogg: 'mp3/giantyuqi.ogg'
