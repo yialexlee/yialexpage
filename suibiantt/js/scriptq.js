7fc7b60f0bded6af2dbb774274b9795b67ca14db
涛浪浮萍yialex lee <44304753+yialexlee@users.noreply.github.com>
2021-07-27 07:25:24 +0800

    Update scriptq.js

--- suibiantt/js/scriptq.js
+++ suibiantt/js/scriptq.js
@@ -130,7 +130,7 @@ ogg: 'mp3/cebsafabellniphada.ogg'
 	// Load playlist
 	for (var i=0; i<playlist.length; i++){
 		var item = playlist[i];
-		$('#playlist').append('<li>'+item.artist+' - '+item.title+'</li>');
+		$('#playlist').append('<li>'+item.title+' - '+item.artist+'</li>');
 	}
 
 	var time = new Date(),
