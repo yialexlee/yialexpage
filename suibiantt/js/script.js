2ad1fd81b758f5b3fc6e66f0ae4affe48fdebb5a
涛浪浮萍yialex lee <44304753+yialexlee@users.noreply.github.com>
2021-07-27 07:22:13 +0800

    Update script.js

--- suibiantt/js/script.js
+++ suibiantt/js/script.js
@@ -4867,7 +4867,7 @@ ogg: 'mp3/yyroe.ogg'
 	// Load playlist
 	for (var i=0; i<playlist.length; i++){
 		var item = playlist[i];
-		$('#playlist').append('<li>'+item.artist+' - '+item.title+'</li>');
+		$('#playlist').append('<li>'+item.title+' - '+item.artist+'</li>');
 	}
 
 	var time = new Date(),
