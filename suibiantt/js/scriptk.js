11bc5e73530f417f5c82ab6bbd4e5814839913a0
涛浪浮萍yialex lee <44304753+yialexlee@users.noreply.github.com>
2021-07-27 07:24:27 +0800

    Update scriptk.js

--- suibiantt/js/scriptk.js
+++ suibiantt/js/scriptk.js
@@ -1371,7 +1371,7 @@ ogg: 'mp3/nightmaresondia.ogg'
 	// Load playlist
 	for (var i=0; i<playlist.length; i++){
 		var item = playlist[i];
-		$('#playlist').append('<li>'+item.artist+' - '+item.title+'</li>');
+		$('#playlist').append('<li>'+item.title+' - '+item.artist+'</li>');
 	}
 
 	var time = new Date(),
