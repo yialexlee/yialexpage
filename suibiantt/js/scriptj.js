a96b3188bcaa9f610993f66d66b713e802b246a6
涛浪浮萍yialex lee <lee52934870@gmail.com>
2022-08-06 18:35:34 +0800

    Update scriptj.js

--- suibiantt/js/scriptj.js
+++ suibiantt/js/scriptj.js
@@ -3009,41 +3009,41 @@ ogg: 'mp3/asoboniziu.ogg'
 title: 'Never Grow Up',
 artist: 'ちゃんみな',
 album: '',
-cover: 'img/nevergrowupchanmina.jpg',
+cover: 'img2/nevergrowupchanmina.jpg',
 mp3: '',
-ogg: 'mp3/nevergrowupchanmina.mp3'
+ogg: 'ogg/nevergrowupchanmina.mp3'
 },
 {
 title: 'A Song Saved to Archived',
 artist: 'ちゃんみな',
 album: 'Never Grow Up',
-cover: 'img/nevergrowupchanmina.jpg',
+cover: 'img2/nevergrowupchanmina.jpg',
 mp3: '',
-ogg: 'mp3/asongsavedtoarchivedchanmina.ogg'
+ogg: 'ogg/asongsavedtoarchivedchanmina.ogg'
 },
 {
 title: 'Believe',
 artist: 'みさき',
 album: '',
-cover: 'img/believemisaki.jpg',
+cover: 'img2/believemisaki.jpg',
 mp3: '',
-ogg: 'mp3/believemisaki.mp3'
+ogg: 'ogg/believemisaki.mp3'
 },
 {
 title: '左右盲',
 artist: 'ヨルシカ',
 album: '',
-cover: 'img/sayumoyorushika.jpg',
+cover: 'img2/sayumoyorushika.jpg',
 mp3: '',
-ogg: 'mp3/sayumoyorushika.mp3'
+ogg: 'ogg/sayumoyorushika.mp3'
 },
 {
 title: 'Wing Wing',
 artist: 'Kep1er',
 album: 'FLY-UP',
-cover: 'img/wingwingkep1er.jpg',
+cover: 'img2/wingwingkep1er.jpg',
 mp3: '',
-ogg: 'mp3/wingwingkep1er.ogg'
+ogg: 'ogg/wingwingkep1er.ogg'
 },
 ];
 
